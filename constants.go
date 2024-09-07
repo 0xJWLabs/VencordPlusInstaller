1da8cdaafd85f384080a1d6b76104dcbf37d779d
0xJWLabs <0xjwlabs@gmail.com>
2024-09-07 15:50:49 +1000

    chore: Update

--- constants.go
+++ constants.go
@@ -13,12 +13,12 @@ import "image/color"
 var InstallerGitHash = "Unknown"
 var InstallerTag = "Unknown"
 
-const ReleaseUrl = "https://api.github.com/repos/RobinRMC/VencordPlus/releases/latest"
+const ReleaseUrl = "https://api.github.com/repos/0xJWLabs/VencordPlus/releases/latest"
 const ReleaseUrlFallback = "https://vencord.dev/releases/vencord"
-const InstallerReleaseUrl = "https://api.github.com/repos/RobinRMC/VencordPlusInstaller/releases/latest"
+const InstallerReleaseUrl = "https://api.github.com/repos/0xJWLabs/VencordPlusInstaller/releases/latest"
 const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
 
-var UserAgent = "VencordPlusInstaller/" + InstallerGitHash + " (https://github.com/RobinRMC/VencordPlusInstaller)"
+var UserAgent = "VencordPlusInstaller/" + InstallerGitHash + " (https://github.com/0xJWLabs/VencordPlusInstaller)"
 
 var (
 	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
